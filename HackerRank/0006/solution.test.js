0fddbf5ea699d7b6e985bf1e4b4fa26b8dd3b702
Vítor Carvalho <GoesToMind@gmail.com>
2018-09-08 21:38:23 +0100

    Plus Minus - Change Test Label

--- HackerRank/0006/solution.test.js
+++ HackerRank/0006/solution.test.js
@@ -1,7 +1,7 @@
 const test = require('tape');
 const solution = require('./solution');
 
-test('0005 - Testcase 0', t => {
+test('0006 - Testcase 0', t => {
    t.deepEqual(
       solution.solveChallenge([-4, 3, -9, 0, 4, 1]),
       ['0.500000', '0.333333', '0.166667'],
